1546aa311a8e8e08bd56b1a2b954b762385c18f1
deadvant <deadvant@gmail.com>
2014-05-06 13:43:49 +0200

    gamematrix

--- scripts/gamematrix.js
+++ scripts/gamematrix.js
@@ -1,5 +1,5 @@
 var GamePositionMatrix = function(){
-	var fields = new Array();
+	this.fields = new Array();
 	
 	this.initFields = function(){
 		for(var i=0; i<10; i++) this.fields.push(new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
